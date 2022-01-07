31d5cfac6f4e3884a82585cb3c43109795a140f8
emtepe35 <emtepe35@gmail.com>
2022-01-07 14:15:27 +0100

    commit nooode

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
-app.get("/", function (req, res) {
+app.get("/data", function (req, res) {
     let o = { imie: "Mateusz", nazwisko: "Tupta", klasa: "3i1", grupa: "gr2" }
     res.send(o)
+})
+
+app.get("/", function (req, res) {
+    res.send("Mój pierwszy heroku")
 })
 
 app.listen(PORT, function () {
